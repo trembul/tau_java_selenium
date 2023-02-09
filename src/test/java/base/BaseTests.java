53f409b1c7a51d9cf8afb8159a18402551d749e3
Jaroslaw Chmurak <smilech78@gmail.com>
2023-02-09 20:39:13 +0100

    WebDriver

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,24 @@
+package base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.manage().window().setSize(new Dimension(580,545));
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
